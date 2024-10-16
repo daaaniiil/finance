cb2250127b860862d45c98b5347ea024504b301b
daaniill <116388121+sunflower560@users.noreply.github.com>
2024-10-16 22:21:08 +0300

    create menu

--- src/layouts/default/MainLayout.vue
+++ src/layouts/default/MainLayout.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="main-layout">
+    <el-drawer
+        class="main-layout__sidebar-drawer"
+
+        v-model="drawerState"
+        :with-header="false"
+        :show-close="false"
+        size="300px"
+        :append-to-body="false"
+        direction="rtl">
+      <div>
+        <div>
+          <el-container class="main-layout__aside-container">
+            <el-header ref="headerAside">
+              <div class="main-layout__header-aside">
+                <img src="../../../public/finance.png" alt="Logo" style="width: 25px; height: 25px">
+                <p>finance</p>
+              </div>
+              <el-button type="danger" @click="drawerState = false">
+                <el-icon class="el-icon--left">
+                  <CircleCloseFilled/>
+                </el-icon>
+                Close
+              </el-button>
+            </el-header>
+            <el-main>
+              <el-scrollbar>
+                <div class="main-layout__aside-content">
+                  <router-link :to="{name:'main-page'}">Главная</router-link>
+                  <router-link :to="{name:'income-expenses-page'}">Доходы & Расходы</router-link>
+                  <router-link :to="{name:'analytics-page'}">Аналитика</router-link>
+                  <router-link :to="{name:'settings-page'}">Настройки</router-link>
+                </div>
+              </el-scrollbar>
+            </el-main>
+            <el-footer>
+              <div class="main-layout__footer">
+                <p>Footer</p>
+              </div>
+            </el-footer>
+          </el-container>
+        </div>
+      </div>
+    </el-drawer>
     <el-container class="main-layout__main-container">
       <el-header height="auto">
         <indent-container>
@@ -11,8 +54,13 @@
               </el-col>
               <el-col :md="12" :lg="21" :xs="12" :sm="18">
                 <div class="nav-links">
-                  <router-link :to="{name:'main-page'}">Главная</router-link>
-                  <router-link :to="{name:'analytics-page'}">Аналитика</router-link>
+                  <!--                  <router-link :to="{name:'main-page'}">Главная</router-link>-->
+                  <!--                  <router-link :to="{name:'analytics-page'}">Аналитика</router-link>-->
+                  <el-button type="primary" @click="drawerState = !drawerState">
+                    <el-icon size="25">
+                      <More/>
+                    </el-icon>
+                  </el-button>
                 </div>
               </el-col>
             </el-row>
@@ -43,16 +91,24 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import IndentContainer from "@/components/containers/IndentContainer.vue";
+import {
+  More,
+  CircleCloseFilled
+} from '@element-plus/icons-vue'
 
 export default defineComponent({
-  name: "EmptyLayout",
-  components: {IndentContainer},
+  name: "MainLayout",
+  components: {IndentContainer, CircleCloseFilled, More},
   setup() {
+    const drawerState = ref(false)
+    const headerAside = ref<HTMLElement | null>(null)
 
-
-    return {}
+    return {
+      drawerState,
+      headerAside
+    }
   }
 })
 </script>
@@ -62,8 +118,16 @@ export default defineComponent({
 
 .main-layout {
 
+  &__sidebar-drawer {
+    .el-drawer__body {
+      height: 100%;
+      padding: 0;
+    }
+  }
+
   &__wrap,
-  &__main-container {
+  &__main-container,
+  &__aside-container {
     height: 100%;
   }
 
@@ -77,10 +141,15 @@ export default defineComponent({
   &__header, &__footer {
     height: 100%;
     align-items: center;
+
     p {
       font-family: "Syne", sans-serif;
       user-select: none;
     }
+  }
+
+  &__main-content {
+    margin-top: $padding;
   }
 
   &__main-container {
@@ -88,6 +157,7 @@ export default defineComponent({
   }
 
   .el-header {
+    border-bottom: $color_border_gray 1px solid;
     padding: $padding 0;
 
     img {
@@ -121,6 +191,38 @@ export default defineComponent({
 
   .el-main {
     padding: 0;
+  }
+
+
+  &__aside-container {
+    height: 100vh;
+
+    .el-header, .el-footer {
+      border-bottom: $color_border_gray 1px solid;
+      border-top: $color_border_gray 1px solid;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-left: $padding - 5;
+      padding-right: $padding - 5;
+      background-color: white;
+    }
+
+    .main-layout__aside-content {
+      padding: $padding - 5;
+      display: flex;
+      flex-direction: column;
+
+      a {
+        margin-bottom: $padding - 5;
+      }
+    }
+  }
+
+  &__header-aside {
+    display: flex;
+    align-items: center;
+    user-select: none;
   }
 }
 </style>
