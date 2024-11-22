f1ea75dee1a1873bdd8b3d61d06dfa6f4a6ceeed
daaniill <116388121+sunflower560@users.noreply.github.com>
2024-11-22 21:37:50 +0300

    change expenses-form categories

--- src/forms/EarningsForm.vue
+++ src/forms/EarningsForm.vue
@@ -67,21 +67,6 @@ const labelPosition = computed(() => {
 
 const currentMonth = new Date().getMonth()
 
-// const months = [
-//   { label: 'Январь', value: 0 },
-//   { label: 'Февраль', value: 1 },
-//   { label: 'Март', value: 2 },
-//   { label: 'Апрель', value: 3 },
-//   { label: 'Май', value: 4 },
-//   { label: 'Июнь', value: 5 },
-//   { label: 'Июль', value: 6 },
-//   { label: 'Август', value: 7 },
-//   { label: 'Сентябрь', value: 8 },
-//   { label: 'Октябрь', value: 9 },
-//   { label: 'Ноябрь', value: 10 },
-//   { label: 'Декабрь', value: 11 },
-// ]
-
 const isMonthDisabled = (monthLabel: string) => {
   const addedMonths: string[] = store.earnings.map((e: IEarnings) => e.month)
   const isMonthAdded: boolean = addedMonths.includes(monthLabel)
