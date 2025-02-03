61b5f7caddaea667bced40b406867e81f59656b8
daaniill <116388121+sunflower560@users.noreply.github.com>
2025-02-03 13:55:11 +0300

    fix completed-goals-view

--- src/views/common/CompletedGoalsView.vue
+++ src/views/common/CompletedGoalsView.vue
@@ -5,8 +5,8 @@
       <el-breadcrumb-item>Настройки</el-breadcrumb-item>
     </el-breadcrumb>
     <Goals :goals="completedGoals" v-if="completedGoals.length" :visible-hidden="false" title="Ваши завершенные цели"/>
-    <Goals :goals="failedGoals" v-if="failedGoals.length" :visible-hidden="false" title="Ваши проваленные цели"/>
     <el-empty description="У вас пока нет завершенных целей" v-else/>
+    <Goals :goals="failedGoals" v-if="failedGoals.length" :visible-hidden="false" title="Ваши проваленные цели"/>
   </div>
 </template>
 
